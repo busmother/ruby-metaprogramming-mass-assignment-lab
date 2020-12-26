07f85a3f97be1fec58bd3e16afb442622b6212d7
Ian Marshall <imarshall14@gmail.com>
2020-12-26 18:53:35 -0500

    commit tree 201d15bfc32d87f9be49d39545ddd912198786d6

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-  #your code here
+  # def initialize(attributes)
+  #   attributes.each {|key, value| self.send(("#{key}="), value)}
+  # end
+  def initialize(attributes)
+    attributes.each do |key, value| 
+      self.class.attr_accessor(key)
+      self.send(("#{key}="), value)
+    end
+  end
+  def name=(name)
+    @name
+  end
 end
